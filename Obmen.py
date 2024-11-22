ee1e6eee6cade51ce0c8def034140496781308e2
Saratsin25 <7422037@gmail.com>
2024-11-22 17:42:30 +0300

    Вместо поля ввода добавлен выпадающий список

--- Obmen.py
+++ Obmen.py
@@ -2,10 +2,12 @@ import requests
 import  json
 from tkinter import *
 from tkinter import messagebox as mb
+from tkinter import ttk
+
 
 
 def exchange():
-    code = entry.get()
+    code = combobox.get()
 
     if code:
         try:
@@ -14,7 +16,7 @@ def exchange():
             data=response.json()
             if code in data['rates']:
                 exchange_rate = data['rates'][code]
-                mb.showinfo("Курс обмена", f"Курс: {exchange_rate} {code} за один доллар")
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rate:.2f} {code} за один доллар")
             else:
                 mb.showerror("Ошибка", f"Валюта {code} не найдена!")
         except Exception as e:
@@ -27,10 +29,14 @@ window = Tk()
 window.title("Курсы обмена валют")
 window.geometry("360x180")
 
-Label(text="Введите код валюты").pack(padx=10, pady=10)
+Label(text="Выберите код валюты").pack(padx=10, pady=10)
+cur = ["RUB", "EUR", "JPY", "GBP", "AUD", "CAD", "CHF", "CNY", "KZT", "UZS"]
+combobox = ttk.Combobox(values=cur)
+combobox.pack(padx=10, pady=10)
 
-entry= Entry()
-entry.pack(padx=10, pady=10)
+
+# entry= Entry()
+# entry.pack(padx=10, pady=10)
 
 Button(text="Получить курс обмена к доллару", command=exchange).pack(padx=10, pady=10)
 
